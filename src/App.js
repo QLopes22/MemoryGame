657c1b6917a8ac80a3440e1cd42a79a71616dadd
Quentin Lopes <lopes.quentin16@gmail.com>
2023-04-18 21:43:30 -0400

    initial commit, completed part 1

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import MemoryCard from './components/MemoryCard';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://play-lh.googleusercontent.com/cJokjWYV_EhTZJvJG0zbV6CowN5V8EoyjzF4LssGyjhTo6rAVntx3XjD7AkBQ2IdFKw" className="App-logo" alt="logo" />
+        <h1>Memory Game</h1>
+        <p class="subheader">Match Memes To Win</p>
+      </header>
+      <div className="card-container">
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/MemoryCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render() {
+        return(
+           <div className="MemoryCard">
+            <img className="card-img" src="https://i.imgflip.com/6bf76l.jpg?a466488"></img>
+           </div>
+        );
+    }
+}
+
+export default MemoryCard;
